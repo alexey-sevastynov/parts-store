9539c3ed83b94d5be537318141d6308a065848c0
Alexey Sevastynov <alexseva94@gmail.com>
2024-04-01 22:57:56 +0300

    added mixin forbid-select-text. and added visible  states isActive links

--- components/modules/Header/Inventory.tsx
+++ components/modules/Header/Inventory.tsx
@@ -3,8 +3,12 @@ import Styles from '@/styles/modules/header/index.module.scss';
 import Link from 'next/link';
 
 import LinkIconDescription from '@/components/elements/LinkIconDescription';
+import React from 'react';
 
 const Inventory = () => {
+  const [isActiveAutoLink, setIsActiveAutoLink] = React.useState<boolean>(true);
+  const [isActiveSaleLink, setIsActiveSaleLink] = React.useState<boolean>(true);
+
   return (
     <ul className={Styles.header__inventory}>
       <li className={Styles.header__inventory_category}>
@@ -17,15 +21,19 @@ const Inventory = () => {
         </LinkIconDescription>
       </li>
 
-      <li className={Styles.header__inventory_auto}>
-        <LinkIconDescription imageName='auto' href={'/auto'} color='light'>
-          Auto
-        </LinkIconDescription>
-      </li>
+      {isActiveAutoLink && (
+        <li className={Styles.header__inventory_auto}>
+          <LinkIconDescription imageName='auto' href={'/auto'} color='light'>
+            Auto
+          </LinkIconDescription>
+        </li>
+      )}
 
-      <li className={Styles.header__inventory_sale}>
-        <Link href={'/sale'}>Sale</Link>
-      </li>
+      {isActiveSaleLink && (
+        <li className={Styles.header__inventory_sale}>
+          <Link href={'/sale'}>Sale</Link>
+        </li>
+      )}
     </ul>
   );
 };
