19ee828003375ac15dd0c024cf148e50e7ff99be
Alexey Sevastynov <alexseva94@gmail.com>
2024-04-03 15:37:45 +0300

    added naviget icons to HeaderIconPanel

--- components/modules/Header/HeaderIconPanel.tsx
+++ components/modules/Header/HeaderIconPanel.tsx
@@ -1,9 +1,42 @@
+import Styles from '@/styles/modules/header/index.module.scss';
+
+import Link from 'next/link';
+
 import LangToggle from '@/components/elements/LangToggle';
+import { BiSolidCarGarage } from 'react-icons/bi';
+import { MdFavorite } from 'react-icons/md';
+import { PiShoppingCartSimpleFill } from 'react-icons/pi';
+import { FaUserLarge } from 'react-icons/fa6';
 
 const HeaderIconPanel = () => {
+  const sizeIcon = 25;
   return (
-    <div>
+    <div className={Styles.headerIconPanel}>
       <LangToggle />
+      <nav className={Styles.headerIconPanel__navigate}>
+        <ul className={Styles.headerIconPanel__navigate_list}>
+          <li>
+            <Link href='/garage'>
+              <BiSolidCarGarage size={sizeIcon} />
+            </Link>
+          </li>
+          <li>
+            <Link href='/favorites'>
+              <MdFavorite size={sizeIcon} />
+            </Link>
+          </li>
+          <li>
+            <Link href='/cart'>
+              <PiShoppingCartSimpleFill size={sizeIcon} />
+            </Link>
+          </li>
+          <li>
+            <Link href='/auth'>
+              <FaUserLarge size={sizeIcon} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
