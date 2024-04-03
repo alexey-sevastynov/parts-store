5bbcb271c3fbe1da9414b3f4c00c322121354837
Alexey Sevastynov <alexseva94@gmail.com>
2024-04-03 11:50:41 +0300

    connected redux-persist library

--- components/layouts/Providers.tsx
+++ components/layouts/Providers.tsx
@@ -2,10 +2,17 @@
 
 import { Provider } from 'react-redux';
 
-import { store } from '@/context/store';
+import { persistor, store } from '@/context/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store()}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default Providers;
