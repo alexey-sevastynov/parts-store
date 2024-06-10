186586d2bc0021629b4a7996bed50f6c15b5f679
Alexey Sevastynov <alexseva94@gmail.com>
2024-06-10 23:39:25 +0300

    added HeaderAdmin to add goods page

--- app/dashboard/goods/add/page.tsx
+++ app/dashboard/goods/add/page.tsx
@@ -1,16 +1,19 @@
 import { getAllBrands } from '@/actions/brandActions';
 import AddPage from '@/components/templates/Dashboard/GoodsPage/AddPage/AddPage';
+import { getUsers } from '@/utils/dashboards';
 
 const Add = async () => {
   try {
     const requiredBrands = await getAllBrands();
+    const fetchedUsers = await getUsers();
 
-    if (requiredBrands.brands)
+    if (requiredBrands.brands && fetchedUsers.users)
       return (
         <AddPage
           brands={requiredBrands.brands}
           statusBrands={requiredBrands.status}
           msgBrands={requiredBrands.msg}
+          data={fetchedUsers}
         />
       );
     else {
--- components/templates/Dashboard/GoodsPage/AddPage/AddPage.tsx
+++ components/templates/Dashboard/GoodsPage/AddPage/AddPage.tsx
@@ -1,19 +1,29 @@
 'use client';
 
 import Add from '@/components/modules/Dashboard/Goods/Add/Add';
+import HeaderAdmin from '@/components/modules/Dashboard/Header/HeaderAdmin';
 import { IBrand } from '@/types/brand';
+import { IUser } from '@/types/user';
 
 const AddPage = ({
   brands,
   brandsStatus,
   brandsMsg,
+  data: { msg, status, users },
 }: {
   brands: IBrand[];
   brandsStatus: number;
   brandsMsg: string;
+  data: { msg: string; status: number; users: IUser[] };
 }) => {
   return (
     <main className='dashboard-pages'>
+      <HeaderAdmin
+        statusDataUsers={status}
+        msgDataUsers={msg}
+        dataUsers={users}
+      />
+
       <Add brands={brands} brandsStatus={brandsStatus} brandsMsg={brandsMsg} />
     </main>
   );
